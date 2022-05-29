ac1cffcb6a9bacd34295e840d519b5a56df3900c
Artyom-Gerchik <bombruchelyator@gmail.com>
2022-05-30 01:31:58 +0300

    tmp

---
+++ LAB10/main.py
@@ -0,0 +1,62 @@
+import time
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def runge_kutta_method(function, n, h, x, y):
+    yn = 0
+    for i in range(n):
+        k1 = h * (function(x, y))
+        k2 = h * (function((x + h / 2), (y + k1 / 2)))
+        k3 = h * (function((x + h / 2), (y + k2 / 2)))
+        k4 = h * (function((x + h), (y + k3)))
+        k = (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        yn = y + k
+        y = yn
+        x = x + h
+
+    return x, yn
+
+
+def adams_bash(funct, n, t0, tn, y0):
+    h = abs(tn - t0) / n
+    t = np.linspace(t0, tn, n + 1)
+    y = np.zeros(n + 1)
+    y[0:3] = runge_kutta_method(funct, 2, t0, t0 + 2 * h, y0)[1]
+    K1 = funct(t[1], y[1])
+    K2 = funct(t[0], y[0])
+    for i in range(2, n):
+        K3 = K2
+        K2 = K1
+        K1 = funct(t[i], y[i])
+        y[i + 1] = y[i] + h * (23 * K1 - 16 * K2 + 5 * K3) / 12
+
+    return tn, y
+
+
+def function(x, y):
+    return 0.7 * (1 - y ** 2) / (2 * x ** 2 + y ** 2 + 1)
+
+
+print("\nМетод Рунге-Кутта:")
+t = time.perf_counter()
+runge_kutta = runge_kutta_method(function, 1000, 0.001, 0, 0)
+t_w = time.perf_counter() - t
+print(f"В точке ({round(runge_kutta[0])}) имеет значение: {'%.6f' % runge_kutta[1]}")
+print(f"Время выполнения: {'%.6f' % t_w}")
+
+print("\nМетод Адамса:")
+t = time.perf_counter()
+adams_bash = adams_bash(function, 1000, 0, 1, 0)
+t_w = time.perf_counter() - t
+print(f"В точке ({adams_bash[0]}) имеет значение:\n {adams_bash[1]}")
+print(f"Время выполнения: {'%.6f' % t_w}")
+
+x_str = [0]
+for i in range(1000, 0, -1):
+    x_str.append(x_str[1000 - i] + 1 / 1000)
+plt.plot(x_str, adams_bash[1], "xb")
+plt.grid(True)
+plt.axis("equal")
+plt.show()
--- LAB9/main.py
+++ LAB9/main.py
@@ -1,119 +1,67 @@
-import numpy as np
-
-count_of_dots = 10 ** 3
-eps = 10 ** -3
-
-
 # # EXAMPLE 1 #
-# def yder(x, y): return x
-#
-#
-# y0 = 0
-# left_border, right_border = -1, 0
-#
-#
-# def ans(x): return (1 / 2) * x ** 2
-#
-#
-# xplot = np.linspace(left_border, right_border, count_of_dots)
-# yplot = [ans(x_dot) for x_dot in xplot]
-#
-#
+# def function(x, y):
+#     return x
 # # EXAMPLE 1 #
 
 
 # # EXAMPLE 2 #
-# def yder(x, y): return x + y
-#
-#
-# y0 = 0
-# left_border, right_border = -1, 1
-#
-#
-# def ans(x): return -x - 1 + np.exp(x)
-#
-#
-# xplot = np.linspace(left_border, right_border, count_of_dots)
-# yplot = [ans(x_dot) for x_dot in xplot]
-#
-#
+# def function(x, y):
+#     return x + y
 # # EXAMPLE 2 #
 
-# MAINT TASK #
-m, a = 1.0, 0.7
+# # MAIN TASK #
+def function(x, y):
+    return (0.7 * (1 - y ** 2)) / ((1 + 1.0) * x ** 2 + y ** 2 + 1)
 
 
-def yder(x, y): return (a * (1 - y ** 2)) / ((1 + m) * x ** 2 + y ** 2 + 1)
+# # MAIN TASK #
 
 
-y0 = 0
-left_border, right_border = 0, 1
+def euler_method(func, n, h, x, y):
+    for i in range(n):
+        y += h * func(x, y)
+        x += h
+    return x, y
 
 
-# MAIN TASK #
+def runge_kutta_method(func, n, h, x, y):
+    yn = 0
+    for i in range(n):
+        k1 = h * (func(x, y))
+        k2 = h * (func((x + h / 2), (y + k1 / 2)))
+        k3 = h * (func((x + h / 2), (y + k2 / 2)))
+        k4 = h * (func((x + h), (y + k3)))
+        k = (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        yn = y + k
+        y = yn
+        x = x + h
+
+    return x, yn
 
 
-def euler_method(x_dot, N):
-    y_dots = [y0]
-    h = x_dot / N
-    for i in range(N):
-        x = i * h
-        y = y_dots[-1]
-        y_dots += [y + h * yder(x, y)]
-    return y_dots
+print("Метод Эйлера и Рунге-Кутта(4-го порядка)\n")
 
+n = 10
+h = 0.1
+x = 0
+y = 0
 
-def runge_kutta_method(x_dot, N):
-    y_dots = [y0]
-    h = x_dot / N
-    for i in range(N):
-        x = i * h
-        y = y_dots[-1]
-        K1 = h * yder(x, y)
-        K2 = h * yder(x + h / 2, y + K1 / 2)
-        K3 = h * yder(x + h / 2, y + K2 / 2)
-        K4 = h * yder(x + h, y + K3)
-        y_dots += [y + 1 / 6 * (K1 + 2 * K2 + 2 * K3 + K4)]
-    return y_dots
+for i in range(7):
+    print('++++++++++++++++++++++++++++++')
+    print()
+    print(f'h = {h:.10f}')
+    print()
 
+    point, answer = euler_method(function, n, h, x, y)
+    print('Метод Эйлера')
+    print(f'В точке {round(point)} Значение = ', "%.6f" % answer)
 
-def get_value_at_chosed_point(method, x):
-    n = 1
-    while True:
-        olddots, newdots = method(x, n), method(x, 2 * n)
-        if max(abs(newdots[2 * i] - olddots[i]) for i in range(n + 1)) < eps:
-            return newdots[-1], 2 * n
+    print()
+    print('Метод Рунге-Кутта(4-го порядка)')
+    point, answer = runge_kutta_method(function, n, h, x, y)
+    print(f'В Точке {round(point)} Значение = ', "%.6f" % answer)
 
-        else:
-            n *= 2
-
-
-def create_dots(method, x_dots):
-    maxn = 0
-    midn = []
-    for x in x_dots:
-        y, n = get_value_at_chosed_point(method, x)
-        maxn = max(maxn, n)
-        midn += [n]
-    midn = sum(midn) / len(x_dots)
-    return midn, maxn
-
-
-print("Методы Эйлера и Рунге-Кутта\n")
-
-print("Точки для вычислений: ", count_of_dots)
-print("Точность: ", eps)
-
-x_dots = [left_border + (right_border - left_border) / count_of_dots * i for i in range(count_of_dots + 1)]
-
-midn, maxn = create_dots(euler_method, x_dots)
-print('\n\nМЕТОД ЭЙЛЕРА:')
-print('Количество отрезков:')
-print(f'Среднее: {midn}')
-print(f'Максимальное: {maxn}')
-
-midn, maxn = create_dots(runge_kutta_method, x_dots)
-print('\n\nМЕТОД РУНГЕ-КУТТА:')
-print('Количество отрезков:')
-print(f'Среднее: {midn}')
-print(f'Максимальное: {maxn}')
+    n *= 10
+    h /= 10
+    print()
+    print('++++++++++++++++++++++++++++++')
